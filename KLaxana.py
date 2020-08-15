584e75c7ddecf53af9ed13e0cb2c765800ebf83f
Oratiko <69038810+Oratiko@users.noreply.github.com>
2020-08-15 11:29:19 +0300

    Add files via upload

---
+++ KLaxana.py
@@ -0,0 +1,10 @@
+# # # # # # # # # # # # #
+# Coder: KP
+# Hμερ: 28/07/2020
+# Last Update: 30/07/2020
+# # # # # # # # # # # # #
+from KLogin import flag
+import KMenu
+
+if flag == True:
+    KMenu()
